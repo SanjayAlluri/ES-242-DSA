72b427ba3cdce6b9b71394ce233d8f73b1d4ed12
Alluri Yashwanth Sanjay Varma <sanjayalluri4545@gmail.com>
2025-11-09 23:57:30 +0530

    Update Prettyprint.c

--- BST/Prettyprint.c
+++ BST/Prettyprint.c
@@ -4,12 +4,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define unreachable()                                      \
-	do {                                                   \
-		fprintf(stderr, "Error: reached unreachable code at %s:%d\n", \
-		        __FILE__, __LINE__);                       \
-		abort();                                           \
-	} while (0)
+void unreachable(){
+	abort();
+}
 
 
 struct bst {
